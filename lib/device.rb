0f7229591fd783ad2234aac2bd1cb15e94bb4d91
Ivan Polchenko <ipzent@gmail.com>
2017-06-16 22:13:19 +0000

    Fixes + version bump to v1.1.5 Beta - Skiping is now properly fixed - Works under Windows - ^ breaks functionality in Unix flavours -    ^ needs a platform check and import ffi or win32ole (only neede for pinging)

--- lib/device.rb
+++ lib/device.rb
@@ -1,7 +1,6 @@
 class Device < Site
   include Support
 
-  # Device.new   only require name, ip, and last portion of the url for fetching register map csv's
   def initialize(name, ip, url_endpoint)
     @name = name
     @ip = ip
@@ -10,29 +9,25 @@ class Device < Site
     @password = ""
     @skiplist = []
   end
-
-  # Create 'master' csv file, process downloaded csv files into one while skipping some registers
-  def fetchMaps(uids, savepath)
-    master = "#{savepath}/#{@name}_#{@ip}_register-map.csv"
-
+  
+  def fetchMaps(uids, savepath, skipList=[])
+    master = "./data/#{savepath}/#{@name}_#{@ip}_register-map.csv"
+    scount = 0
+    icount = 0
     # Create master device csv file
     begin
       CSV.open(master, 'w') do |row|
-        scount = 0
-        icount = 0
-        
         # Insert header row
         row << ["Parameter Name","Display Name","uid","Base Address (0-based)","Discrete","Units","Type","Size (bytes)","Is Timestamp","Bit Offset","Swap Bytes","Swap Words","Divider","Multiplier","Date Spec","Alarm On False","Writable","Possible Values"]
         puts
         puts "#{' '*(name.length+1)}Fetching individual register maps for Unit IDs: "
         print "#{' '*(name.length+1)}"
-        raise "Empty UIDS" if uids.empty?
-        
         # Iterate over each uid
+        raise "Empty UIDS" if uids.empty?
         uids.each do |uid|
           full_url = "#{url}#{uid}"
           print "#{uid.to_s.blue}: "
-
+        
           # download each file, process and add to master
           downloadFile full_url do |file|
             raise "Received empty file for Unit ID #{uid}." if file.empty?
@@ -41,45 +36,53 @@ class Device < Site
             contents.slice!(0) # remove header row
             if contents.length == 0
               PrintLine.error('none')
-              scount += 1
+              scount = scount + 1
               next
             else
               contents.each do |r|
                 r.insert(2, uid)  # add uid field before the register address field
-                icount += 1
-                
-                # filter out skipped
-                if skiplist.include?(r[1])
-                  scount += 1
-                  next
-                else
-                  row << r
-                end
+                row << r unless skip!(r)
+                icount = icount + 1
               end
               PrintLine.done(contents.count.to_s)
             end
           end
           print "; "
         end
-        puts "\n#{' '*(name.length+1)}Saved: #{master.green}"
+        puts "\n#{' '*(name.length+1)}Saved: #{master.green}" 
         print "#{' '*(name.length+1)}"
-        if (icount-scount) <= 1000
-          print "Total data points: #{(icount-scount).to_s.green}"
+        if icount <= 1000
+          print "Total data points: #{icount.to_s.green}  Skipped: #{scount}\n"
         else
-          print "Total data points: #{(icount-scount).to_s.yellow}"
+          print "Total data points: #{icount.to_s.yellow}  Skipped: #{scount}\n"
         end
-        print "  Skipped: #{scount.to_s.green}\n"
       end
     rescue => e
       strace(e,"Something happened with fetching this file")
     end
   end
   
-  # Check if device is reachable on the network, returns boolean true or false
+  # 
+  def skip!(data=[])
+    scount = 0
+    if data==[]
+      return false
+    else
+      skiplist.each do |s|        
+        scount = scount + 1 if data.include? s
+      end
+    end
+    if scount > 0
+      return true
+    else
+      return false
+    end
+  end
+  
   def up?
     check = Net::Ping::WMI.new(@ip)
     check.ping?
   end
-
+  
   attr_accessor :name, :ip, :url, :username, :password, :skiplist
 end
--- lib/site.rb
+++ lib/site.rb
@@ -1,44 +1,32 @@
 class Site
   include Support
-
-  # Site.new(site hash read from site difinition file (yml file)
+  
   def initialize(site)
     begin
       @name = site[:name]
       @uids = site[:uids]
       @username = site[:username]
       @password = site[:password]
-      @savedir = site[:name]
       @savepath = "./data/#{@name}"
       @devicesfile = site[:devicesFile]
       @endpointurl = site[:endpointUrl]
-      @skiplist = []
+      @skiplist = site[:skipRegisters]
       @devices = []
-      site[:skipRegisters].each do |s|
-        if !s.instance_of? String
-          @skiplist.push s.to_s
-        else
-          @skiplist.push s
-        end
-      end
     rescue => e
       puts e.message
       strace(e)
     end
   end
-
-  # Read list of devices from csv file specified in yml SDF
-  # Populate 'devices' array with device objects
+  
   def updateDeviceList
     @devices = []
-    PrintLine.updating('Devices',@name.upcase)
+    PrintLine.updating('Devices',@name)
     begin
       dlist = SmarterCSV.process("./sites/#{@devicesfile}")
       dlist.each do |d|
         curdev = Device.new(d[:name],d[:ip],@endpointurl)
         curdev.username = @username
         curdev.password = @password
-        curdev.skiplist = @skiplist
         @devices.push curdev
       end
       print "    Found: "
@@ -46,21 +34,17 @@ class Site
     rescue => e
       strace(e,"Failed to get list of devices")
     end
-  end
-
-  # Iterate through list of all devices and fetch and process csv files
-  # Check if device is reachable on the network before calling
-  # 'fetchMaps' method on each device which actually does most of the work
+  end  
+  
   def fetchMaps
     print "Downloading Register Maps for #{@name.upcase} with uids: ".black.on_white
     @uids.each {|u| print "#{u}; ".black.on_white}; puts
     begin
-      savepath = "./#{@savedir}"
-      @devices.each do |d|
+      @devices.each do |d| 
         print "Device: #{d.name.magenta} IP: #{d.ip.cyan} #{' ' * 10}".on_black
         if d.up?
           PrintLine.done " up "
-          d.fetchMaps(@uids, savepath)
+          d.fetchMaps(@uids, @savepath, @skiplist)
         else
           PrintLine.error ' downn '
           puts
@@ -71,6 +55,6 @@ class Site
       strace(e, 'Failed to fetch maps from all devices')
     end
   end
-
+  
   attr_reader :name, :uids, :username, :password, :devicesfile, :endpointurl, :devices, :skiplist
 end
