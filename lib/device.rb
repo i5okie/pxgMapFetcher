d467468ba0860b202293fded98b4efcc247bca2a
Ivan Polchenko <ipzent@gmail.com>
2017-06-16 20:52:36 +0000

    Fixed skip logic - Changed skiplist array from numeric+alphanumeric array, to string only arry - Changed skip logic to check single value out of row against skiplist.

--- lib/device.rb
+++ lib/device.rb
@@ -13,20 +13,22 @@ class Device < Site
 
   # Create 'master' csv file, process downloaded csv files into one while skipping some registers
   def fetchMaps(uids, savepath, skipList=[])
-    master = "./data/#{savepath}/#{@name}_#{@ip}_register-map.csv"
-    scount = 0
-    icount = 0
+    master = "#{savepath}/#{@name}_#{@ip}_register-map.csv"
 
     # Create master device csv file
     begin
       CSV.open(master, 'w') do |row|
+        scount = 0
+        icount = 0
+        
         # Insert header row
         row << ["Parameter Name","Display Name","uid","Base Address (0-based)","Discrete","Units","Type","Size (bytes)","Is Timestamp","Bit Offset","Swap Bytes","Swap Words","Divider","Multiplier","Date Spec","Alarm On False","Writable","Possible Values"]
         puts
         puts "#{' '*(name.length+1)}Fetching individual register maps for Unit IDs: "
         print "#{' '*(name.length+1)}"
-        # Iterate over each uid
         raise "Empty UIDS" if uids.empty?
+        
+        # Iterate over each uid
         uids.each do |uid|
           full_url = "#{url}#{uid}"
           print "#{uid.to_s.blue}: "
@@ -39,13 +41,20 @@ class Device < Site
             contents.slice!(0) # remove header row
             if contents.length == 0
               PrintLine.error('none')
-              scount = scount + 1
+              scount += 1
               next
             else
               contents.each do |r|
                 r.insert(2, uid)  # add uid field before the register address field
-                row << r unless skip!(r)
-                icount = icount + 1
+                icount += 1
+                
+                # filter out skipped
+                if skiplist.include?(r[1])
+                  scount += 1
+                  next
+                else
+                  row << r
+                end
               end
               PrintLine.done(contents.count.to_s)
             end
@@ -54,35 +63,18 @@ class Device < Site
         end
         puts "\n#{' '*(name.length+1)}Saved: #{master.green}"
         print "#{' '*(name.length+1)}"
-        if icount <= 1000
-          print "Total data points: #{icount.to_s.green}  Skipped: #{scount}\n"
+        if (icount-scount) <= 1000
+          print "Total data points: #{(icount-scount).to_s.green}"
         else
-          print "Total data points: #{icount.to_s.yellow}  Skipped: #{scount}\n"
+          print "Total data points: #{(icount-scount).to_s.yellow}"
         end
+        print "  Skipped: #{scount.to_s.green}\n"
       end
     rescue => e
       strace(e,"Something happened with fetching this file")
     end
   end
-
-  # reads array of values to match against each row of incoming data
-  # returns boolean true or false
-  def skip!(data=[])
-    scount = 0
-    if data==[]
-      return false
-    else
-      skiplist.each do |s|
-        scount = scount + 1 if data.include? s
-      end
-    end
-    if scount > 0
-      return true
-    else
-      return false
-    end
-  end
-
+  
   # Check if device is reachable on the network, returns boolean true or false
   def up?
     check = Net::Ping::WMI.new(@ip)
